3633f243ad3dcb17155c08eecc8623fa304db5e5
Cliff <cbrohal@gmail.com>
2020-10-06 00:08:55 -0400

    Needs output format cleaned up.

--- StringFormattingProgram.py
+++ StringFormattingProgram.py
@@ -16,31 +16,50 @@ Clifford B. Rohal
 
 FileName = "Names.txt"
 DataFile = open(FileName, 'r')
-List = DataFile.read()
+FileData = DataFile.read()
+List = [FileData]
+
 
 #Output the list in file: Names
 def DataList():
     print("--------------------------------")
     print("Names in file Names.txt:")
-    print(List)
+    print(FileData)
+    print("--------------------------------")
+    print("Names list in file Names.txt:")
+    print (List)
+    print()
 
 #Modify the data by adding "Good eveing dr."
 def GoodEvening():
-    LastName = "F"
-    for line in List:
+    FirstName = ""
+    LastName = ""
+    Index = 0
+    FNBeg = 0
+    FNEnd = 0
+    print("--------------------------------")
+    print("String formatting program output:")
+    print()
+    for line in FileData:
+
+        if line != " ":
+            FirstName += line
+
         if line == " ":
-            print("Good eveing Dr. ")
-            LastName = "T"
-        if LastName == "T":
-            print(line)
-        if line == "":
-          LastName =="F"
-    
-    DataFile.close()
+            FNEnd = Index #Capturing the index of the end of the first name
+            
+        if line != "\n":
+            LastName += line
+            Index += 1 #Index counts here to prevent counting a carriage return
+
+        if line == "\n":
+            print("Good eveing Dr.",LastName[FNEnd:Index] , "Would you mind if I call you", FirstName[FNBeg:FNEnd], "?")
+            FNBeg = Index #Capturing the index of the beginning of the first name
 
 def Main():
     DataList()
     GoodEvening()
+    DataFile.close()
 
 Main() 
 
